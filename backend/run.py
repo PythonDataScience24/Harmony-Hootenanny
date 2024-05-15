35733ed27a629682cb80e52bb63a9198299274dd
Manuel <manuel.flueckiger.mf@gmail.com>
2024-05-15 13:52:01 +0200

    removed it, init

--- backend/run.py
+++ backend/run.py
@@ -4,7 +4,6 @@ from database import init_db
 from harmonyhootenanny import create_app, socketio
 
 if __name__ == "__main__":
-    init_db()
     app = create_app()
     CORS(app,resources={r"/*":{"origins":"*"}})
     socketio.run(app)
